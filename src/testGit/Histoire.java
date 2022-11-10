fb599e19f25a04480077c4c71a9130da5015a757
RotsyArilandy <117807459+RotsyArilandy@users.noreply.github.com>
2022-11-10 16:11:14 +0100

    Update Histoire.java

--- src/testGit/Histoire.java
+++ src/testGit/Histoire.java
@@ -3,8 +3,8 @@ package testGit;
 public class Histoire {
 	
 	public static void main (String [] args ) {
-		System.out.println("Test Git");
-		System.out.println("Test Git");
+		System.out.println("Test Git2");
+		System.out.println("Test Git3");
 		
 	}
 }
