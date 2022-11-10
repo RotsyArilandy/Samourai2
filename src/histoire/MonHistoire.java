6592167b7537e06301a677b1ff26e97f1bc0c908
Rotsy <Rotsy@LAPTOP-LQ20HN6O>
2022-11-10 16:51:01 +0100

    Test 1 Humain - Réussite

--- src/histoire/MonHistoire.java
+++ src/histoire/MonHistoire.java
@@ -1,10 +1,17 @@
 package histoire;
 
+import personnages.Humain;
+
 public class MonHistoire {
 	
 	public static void main (String [] args ) {
-		System.out.println("Test Git2");
-		System.out.println("Test Git3");
+		Humain h = new Humain("Rotsy",500,"thé");
+		h.direBonjour();
+		h.parler("J'ai "+ h.getArgent());
+		h.gagnerArgent(40);
+		h.parler("J'ai "+ h.getArgent());
+		h.perdreArgent(50);
+		h.parler("J'ai "+ h.getArgent());
 		
 	}
 }
