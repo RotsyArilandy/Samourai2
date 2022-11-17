1706b128eaed9865417227a9f43929a43d48fd57
Rotsy <Rotsy@LAPTOP-LQ20HN6O>
2022-11-17 22:49:45 +0100

    Modification Classe Ninja

--- src/personnages/secondaires/Ninja.java
+++ src/personnages/secondaires/Ninja.java
@@ -6,6 +6,7 @@ import personnages.principaux.*;
 
 public class Ninja extends Yakuza{
 	
+	private String clanSecret;
 	private static final String[] clansNinja =  { "of Shadows","of Mist", "of Clouds", "of Fog","of Darkness"};
 
 	public Ninja(String nom, int argent, String boisson, String clan) {
@@ -13,22 +14,21 @@ public class Ninja extends Yakuza{
 	}
 	
 	public void direBonjour() {
-		String cl = "";
 		Random r = new Random();
 		int valeur = r.nextInt(1,5);
 		switch (valeur) {
 			case 1 : 
-				cl = clansNinja[1];
+				clanSecret = clansNinja[1];
 				break;
 			case 2 :
-				cl = clansNinja[2];
+				clanSecret  = clansNinja[2];
 			case 3 :
-				cl = clansNinja[3];
+				clanSecret  = clansNinja[3];
 			case 4 :
-				cl = clansNinja[4];
+				clanSecret  = clansNinja[4];
 		}
 		super.direBonjour();
-		parler("mon clan secret est "+this.getClan()+".\n (mon clan secret est "+this.getClan()+" "+cl+ "et maintenant que tu les sais, je vais devoir te tuer");
+		parler("mon clan secret est "+this.getClan()+".\n (mon clan secret est "+this.getClan()+" "+this.clanSecret+ "et maintenant que tu les sais, je vais devoir te tuer");
 	}
 	
 	
